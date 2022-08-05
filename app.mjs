050c8f1327decc91b5c46294628e0491cbdf0b7a
Favour  Timothy <45835081+wyenelle@users.noreply.github.com>
2022-08-05 17:04:39 +0000

    first commit on redactr

--- app.mjs
+++ app.mjs
@@ -1,17 +1,23 @@
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
-  
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
+  function getData() {
+  let text = document.getElementById("text").value.split(" ");
+  let asterisk = "*";
+  let repText = document.getElementById("scramble").value.split(" ");
+
+  const indexes = [];
+  let result 
+
+  for (let index = 0; index < text.length; index++) {
+    if (text[index] === repText[0] || text[index] === repText[1]) {
+      indexes.push(index);
+     result = text.splice(index,1,asterisk.repeat(text[index].length))
     }
-  };
-  
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
+
+  }
+ document.getElementById('demo').innerText = text.join(' ')
+ 
+}
+};
+// ======= DO NOT EDIT ============== //
+export default startApp;
   // ======= EEND DO NOT EDIT ========= //
