a74d05e53cdc6fdd79db896f1a2f7737c35caba3
Favour  Timothy <45835081+wyenelle@users.noreply.github.com>
2022-08-06 06:25:44 +0000

    .

--- app.mjs
+++ app.mjs
@@ -1,22 +1,6 @@
+import getData from './index.mjs'
 function startApp() {
-  function getData() {
-  let text = document.getElementById("text").value.split(" ");
-  let asterisk = "*";
-  let repText = document.getElementById("scramble").value.split(" ");
-
-  const indexes = [];
-  let result 
-
-  for (let index = 0; index < text.length; index++) {
-    if (text[index] === repText[0] || text[index] === repText[1]) {
-      indexes.push(index);
-     result = text.splice(index,1,asterisk.repeat(text[index].length))
-    }
-
-  }
- document.getElementById('demo').innerText = text.join(' ')
- 
-}
+  getData()
 };
 // ======= DO NOT EDIT ============== //
 export default startApp;
--- index.mjs
+++ index.mjs
@@ -1,4 +1,21 @@
 import startApp from './app.mjs';
+function getData() {
+  let text = document.getElementById("text").value.split(" ");
+  let asterisk = "*";
+  let repText = document.getElementById("scramble").value.split(" ");
 
+  const indexes = [];
+  let result
 
-document.addEventListener('DOMContentLoaded', startApp); 
+  for (let index = 0; index < text.length; index++) {
+    if (text[index] === repText[0] || text[index] === repText[1]) {
+      indexes.push(index);
+      result = text.splice(index, 1, asterisk.repeat(text[index].length))
+    }
+
+  }
+  document.getElementById('demo').innerText = text.join(' ')
+
+}
+export default getData
+document.addEventListener('DOMContentLoaded', startApp);
