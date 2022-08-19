1c8b202b8d85b55482ad4649f4a97505a65b196d
Favour  Timothy <favourkcb@gmail.com>
2022-08-19 05:22:28 +0100

    'Done'

--- index.mjs
+++ index.mjs
@@ -15,14 +15,18 @@ function resetInput(){
   document.getElementById("text").value = ''
   document.getElementById("scramble").value = ''
   document.getElementById('report').innerHTML = ''
+  document.getElementById('demo').innerText = ''
   
   
 }
+
+ 
+
 //    This function is called pnce the 'Redactr' is clicked  
 function getData() {
   let character = prompt('what character would you love to see your scrambled text in')
-  let text = document.getElementById("text").value.split(" "); //  words are split into arrays for easier manipulations
-  let asterisk = "*";
+  let text = document.getElementById("text").value.trim().split(" "); //  words are split into arrays for easier manipulations
+  
   let repText = document.getElementById("scramble").value.split(" ");
 
 
@@ -31,25 +35,31 @@ function getData() {
   // this is where we compare for matches 
 
   for (let index = 0; index < text.length; index++) {
-    if (repText.includes(text[index]))  {
+    if (repText.includes(text[index]) )  {
       
       //replacing the matched words
       // splice doesn't use index-based counting, so it allowed us delete and replace same particular value using the value of index
        text.splice(index, 1, character.length > 1 ? character : character.repeat(text[index].length))
        indexes.push(index)
-       
-    }
+
     
+        if(indexes[index] != undefined || indexes[index] !== '')     
+       indexes.pop()
+
+    }
   }
+
+
+
   // this values are going into the report section
   let rand = Math.random()
-  let report = repText.join('')
 
+
+// scrambled text goes in here
   document.getElementById('demo').innerText = text.join(' ')
-  document.getElementById('report').innerHTML = `${repText.length} ${repText.length > 1 ? 'words were' : 'word was' }  scanned <br/> In total, ${indexes.length} ${indexes.length > 1 ? 'words were' : 'word was' } scrambled <br/> <br/> ${report.length} ${repText.length > 1 ? 'words were' : 'word was' }  scrambled in total in ${rand.toFixed(2)} seconds` 
-
+  document.getElementById('report').innerHTML = `${text.length } ${text.length > 1 ? 'words were' : 'word was' }  scanned <br/> In total, ${indexes.length} ${indexes.length > 1 ? 'words were' : 'word was' } scrambled  in ${rand.toFixed(2)} seconds`
   
- /// that was a handful..lol
+ // that was a handful..lol
   
  
 }
